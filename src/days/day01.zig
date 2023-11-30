f01feb75abe9c26878c8318d1f58f41be13d1002
Kalle Fagerberg <kalle.f8+git@proton.me>
2023-11-30 20:34:35 +0100

    Removed excess test

--- src/days/day01.zig
+++ src/days/day01.zig
@@ -3,8 +3,3 @@ const std = @import("std");
 pub fn part1() !void {
     std.debug.print("day01 part1 :)\n", .{});
 }
-
-test "day01 foobar" {
-    const value = 1 + 2;
-    try std.testing.expectEqual(3, value);
-}
