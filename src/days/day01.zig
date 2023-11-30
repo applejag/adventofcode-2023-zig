58ed3fb057cf6db7e0bdcb8f8986e5628259c0b8
Kalle Fagerberg <kalle.f8+git@proton.me>
2023-12-01 00:11:09 +0100

    Day 1, part 1

--- src/days/day01.zig
+++ src/days/day01.zig
@@ -9,5 +9,45 @@ pub fn part1(allocator: std.mem.Allocator, input: []u8) !void {
 
     std.debug.print("day01 part1 :)\n", .{});
 
-    std.debug.print("Some text from input: {s}\n", .{input[0..50]});
+    var sum: i32 = 0;
+
+    const stripped = std.mem.trim(u8, input, "\n ");
+    var lines_iter = std.mem.split(u8, stripped, "\n");
+    while (lines_iter.next()) |line| {
+        const first_digit = std.mem.indexOfAny(u8, line, "0123456789");
+        const last_digit = std.mem.lastIndexOfAny(u8, line, "0123456789");
+
+        if (first_digit == null) {
+            std.log.warn("Missing first digit in line: '{s}'", .{line});
+            continue;
+        }
+        if (last_digit == null) {
+            std.log.warn("Missing last digit in line: '{s}'", .{line});
+            continue;
+        }
+
+        const num = parseDigitPair(line[first_digit.?], line[last_digit.?]) catch |err| {
+            std.log.warn("Failed to parse digits in line: '{s}', {}", .{ line, err });
+            continue;
+        };
+
+        std.log.info("Line: '{s}', number: {d}", .{ line, num });
+        sum += num;
+    }
+
+    std.log.info("Sum = {d}", .{sum});
+}
+
+fn parseDigitPair(first: u8, second: u8) !u8 {
+    const first_num = try parseDigit(first);
+    const last_num = try parseDigit(second);
+
+    return first_num * 10 + last_num;
+}
+
+fn parseDigit(digit: u8) !u8 {
+    return switch (digit) {
+        '0'...'9' => digit - '0',
+        else => error.InvalidArgument,
+    };
 }
